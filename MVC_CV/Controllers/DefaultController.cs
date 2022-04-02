dfdeaf1bb2f07713a0affb9a85158d40ccf84200
Tuğrul Burak Gözdemir <tgrlgzdmr@gmail.com>
2022-04-02 18:36:10 +0300

    Add project files.

---
+++ MVC_CV/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CV.Models.Entity;
+
+namespace MVC_CV.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCVEntities db = new DbCVEntities();
+        public ActionResult Index()
+        {
+            var values = db.Tbl_About.ToList();
+            return View(values);
+        }
+    }
+}
