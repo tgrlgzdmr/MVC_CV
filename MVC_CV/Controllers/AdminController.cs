bbd04cc431d9f3c7acfbd0381b18b55621d7c0c2
Tuğrul Burak Gözdemir <tgrlgzdmr@gmail.com>
2022-04-16 02:26:28 +0300

    Publish

---
+++ MVC_CV/Controllers/AdminController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CV.Models.Entity;
+using MVC_CV.Repositories;
+
+namespace MVC_CV.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        GenericRepository<Tbl_Admin> repo = new GenericRepository<Tbl_Admin>();
+        public ActionResult Index()
+        {
+            var admin= repo.List();
+            return View(admin);
+        }
+
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAdmin(Tbl_Admin p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteAdmin(int id)
+        {
+            Tbl_Admin t = repo.Find(x => x.Id == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateAdmin(int id)
+        {
+            Tbl_Admin t = repo.Find(x => x.Id == id);
+            return View(t);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateAdmin(Tbl_Admin p)
+        {
+            Tbl_Admin t = repo.Find(x => x.Id == p.Id);
+            t.UserName= p.UserName;
+            t.Password = p.Password;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
